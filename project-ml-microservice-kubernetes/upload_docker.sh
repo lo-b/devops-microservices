838dfdb876d93560230b4616a24b5f7e8d006f9c
lo-b <bram_lo@outlook.com>
2020-07-31 23:21:11 +0200

    Add steps to upload built image to docker hub

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath="bramlo/udacity-project-microservices-kubernetes:latest"
 # Step 2:  
 # Authenticate & tag
+docker login
+# tagging already done when image is built
+
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker image push $dockerpath
