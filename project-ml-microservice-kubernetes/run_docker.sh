d625110ce36a071ec4700275091981914c3d24d1
lo-b <bram_lo@outlook.com>
2020-07-31 22:54:19 +0200

    Update docker script
    Add steps to docker script to build and run the docker image.

--- project-ml-microservice-kubernetes/run_docker.sh
+++ project-ml-microservice-kubernetes/run_docker.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
 ## Complete the following steps to get Docker running locally
+TAG="bramlo/udacity-project-microservices-kubernetes:latest"
 
 # Step 1:
 # Build image and add a descriptive tag
-
+docker build -t ${TAG} .
 # Step 2: 
 # List docker images
+docker image ls
 
 # Step 3: 
 # Run flask app
+docker container run -p 8000:80 ${TAG}
