cebf6b20a1db1f4e6b63068281859bf2f7d1dff9
lo-b <bram_lo@outlook.com>
2020-08-01 01:30:26 +0200

    Add steps to running image in k8s cluster

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="bramlo/udacity-project-microservices-kubernetes:latest"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl create deployment udacity-project-deployment --image=$dockerpath 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward deployment/udacity-project-deployment 8000:80
